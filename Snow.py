06ff338e10be6b5effaccc1cc38376829608c7fe
ilySnowade <53481719+ilySnowade@users.noreply.github.com>
2019-08-08 16:09:57 +0800

    Update Snow.py

--- Snow.py
+++ Snow.py
@@ -42,5 +42,5 @@ async def cat(ctx):
     await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
 
     
-bot.run('BOT_TOKEN')
+bot.run('NjA4MjA0MTU0OTEzMTYxMjI2.XUvKCQ.phvJoX628xS_6EsfKGGsvWFfrEw')
 
