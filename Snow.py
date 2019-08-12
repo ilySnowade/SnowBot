c656f9a9544b046fa08fcf1f5486b909358683dc
SnowPls <53481719+ilySnowade@users.noreply.github.com>
2019-08-12 17:42:34 +0800

    Update Snow.py

--- Snow.py
+++ Snow.py
@@ -41,6 +41,16 @@ async def greet(ctx):
 @bot.command()
 async def cat(ctx):
     await ctx.send("https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif")
+  @bot.command(pass_context = True)
+async def mute(ctx, member: discord.Member):
+     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '194151340090327041':
+        role = discord.utils.get(member.server.roles, name='Muted')
+        await bot.add_roles(member, role)
+        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
+        await bot.say(embed=embed)
+     else:
+        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
+        await bot.say(embed=embed)
 
     
 bot.run(os.getenv('BOT_TOKEN'))
