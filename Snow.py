178d6366df24fcb0eb3c6ce936f818dff562e576
SnowPls <53481719+ilySnowade@users.noreply.github.com>
2019-08-12 09:08:09 +0200

    Update Snow.py

--- Snow.py
+++ Snow.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
- 
+import os
  
 client = discord.Client
 bot = commands.Bot(command_prefix='/')
